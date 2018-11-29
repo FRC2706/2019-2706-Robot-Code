2b6148a787a8ea80eacde88e2b5319571f4db331
eandr127 <eandr127@outlook.com>
2018-11-28 20:15:14 -0500

    Revert to alpha gradle Since beta is closed to the public we need to be able to deploy with some 2019 software, so we went with the alpha

--- src/main/java/ca/team2706/frc/robot/Robot.java
+++ src/main/java/ca/team2706/frc/robot/Robot.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 public class Robot extends TimedRobot {
 
     public static void main(String[] args) {
-        RobotBase.startRobot(Robot::new);
+        //RobotBase.startRobot(Robot::new);
     }
 
 }

170fc12236cd8cec713f843f2abe8924ed295680
eandr127 <eandr127@outlook.com>
2018-12-07 19:55:40 -0500

    Clean up Robot.java Implement all methods and add all callbacks. Rearrange method order. Subsystems should be declared above robotInit(), and assigned in robotInit(). They can be accessed using a getter method.

--- src/main/java/ca/team2706/frc/robot/Robot.java
+++ src/main/java/ca/team2706/frc/robot/Robot.java
@@ -1,17 +1,78 @@
 package ca.team2706.frc.robot;
 
-import ca.team2706.frc.robot.config.Config;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 import java.util.ArrayList;
 
 public class Robot extends TimedRobot {
 
-    public static void main(String[] args) {
-//        RobotBase.startRobot(Robot::new);
+    @Override
+    public void robotInit() {
+        onStateChange(RobotState.ROBOT_INIT);
     }
 
+    /**
+     * Called when the robot enters the disabled state.
+     */
+    @Override
+    public void disabledInit() {
+        if(LiveWindow.isEnabled()) {
+            LiveWindow.setEnabled(false);
+        }
+
+        // Iterate through each of the state-change listeners and call them.
+        onStateChange(RobotState.DISABLED);
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void autonomousInit() {
+        // Iterate through each of the state-change listeners and call them.
+        onStateChange(RobotState.AUTONOMOUS);
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void teleopInit() {
+        // Iterate through each of the state-change listeners and call them.
+        onStateChange(RobotState.TELEOP);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void testInit() {
+        LiveWindow.setEnabled(true);
+
+        // Iterate through each of the state-change listeners and call them.
+        onStateChange(RobotState.TEST);
+    }
+
+    @Override
+    public void testPeriodic() { }
+
+
     private static final ArrayList<StateConsumer> STATE_LISTENERS = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(Robot::shutdown));
+
+        // TODO: Uncomment when using WPILib beta
+//        RobotBase.startRobot(Robot::new);
+    }
 
     /**
      * Sets the given listener to be called when the robot is disabled.
@@ -22,33 +83,15 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * Called when the robot enters the disabled state.
-     */
-    @Override
-    public void disabledInit() {
-        // Iterate through each of the disabled listeners and call them.
-        onStateChange(RobotState.DISABLED);
-    }
-
-    @Override
-    public void disabledPeriodic() {
-    }
-
-    @Override
-    public void teleopPeriodic() {
-    }
-
-    @Override
-    public void robotInit() {
-        Config.initialize();
-        onStateChange(RobotState.ROBOT_INIT);
-    }
-
-    /**
      * Calls the state change event, executing the listeners.
      * @param newState The robot's current (new) state.
      */
     private static void onStateChange(RobotState newState) {
         STATE_LISTENERS.forEach(action -> action.accept(newState));
+    }
+
+    private static void shutdown() {
+        // Iterate through each of the state-change listeners and call them.
+        Robot.onStateChange(RobotState.SHUTDOWN);
     }
 }

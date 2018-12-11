ce1aa6b50a1c9f0328ed556a2aae053052b1ff80
Kyle <kra.aue@gmail.com>
2018-12-11 17:45:58 -0500

    Add comments
    Just wanted to add some comments to the main class, since rookies will
    be seeing it a lot and may need to know what some of the methods do.

--- src/main/java/ca/team2706/frc/robot/StateConsumer.java
+++ src/main/java/ca/team2706/frc/robot/StateConsumer.java
@@ -1,5 +1,8 @@
 package ca.team2706.frc.robot;
 
+/**
+ * Essentially the same thing as {@code Consumer<RobotState>}
+ */
 @FunctionalInterface
 public interface StateConsumer {
     void accept(RobotState state);

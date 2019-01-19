8e6429bd81a39c0604893cf37375d0e889d918c4
Kyle Anderson <kra.aue@gmail.com>
2019-01-19 12:13:18 -0500

    Fix type
    Just wanted to fix a quick typo.

--- src/main/java/ca/team2706/frc/robot/config/FluidConstant.java
+++ src/main/java/ca/team2706/frc/robot/config/FluidConstant.java
@@ -52,7 +52,7 @@ public class FluidConstant<A> {
                 ntEntry.setValue(value());
 
 
-                // add a listener so we can change update the value.
+                // Add a listener so we can change update the value.
                 ntEntry.addListener(entryNotification -> setValue(ntEntry.getValue()), EntryListenerFlags.kUpdate);
             }
         }

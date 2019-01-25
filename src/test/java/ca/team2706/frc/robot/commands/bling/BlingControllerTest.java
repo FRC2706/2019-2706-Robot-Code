6a308a933bb2adc6c7ffb2c45cd7449e6191e1e3
Kyle Anderson <kra.aue@gmail.com>
2019-01-25 14:52:33 -0500

    Stuff more tests

--- src/test/java/ca/team2706/frc/robot/commands/bling/BlingControllerTest.java
+++ src/test/java/ca/team2706/frc/robot/commands/bling/BlingControllerTest.java
@@ -14,51 +14,46 @@ import static org.junit.Assert.assertEquals;
 
 public class BlingControllerTest {
 
-//    @Tested
-//    private BlingController blingController;
-//
-//    // Since DriverStation is accessed statically (it's a singleton), we have to mock all instances of it.
-//    @Mocked
-//    private DriverStation station;
+    @Tested
+    private BlingController blingController;
 
-    @Test
-    public void testTest() {
-        assertTrue(true);
+    // Since DriverStation is accessed statically (it's a singleton), we have to mock all instances of it.
+    @Mocked
+    private DriverStation station;
+
+    @Before
+    public void setUp() {
+        blingController = new BlingController();
     }
 
-//    @Before
-//    public void setUp() {
-//        blingController = new BlingController();
-//    }
+    /**
+     * Tests getting the robot's operation period while not in a real match.
+     */
+    @Test
+    public void testGetOperationPeriodDuringRealMatch() {
+        new Expectations() {{
+            station.isFMSAttached();
+            result = true;
 
-//    /**
-//     * Tests getting the robot's operation period while not in a real match.
-//     */
-//    @Test
-//    public void testGetOperationPeriodDuringRealMatch() {
-//        new Expectations() {{
-//            station.isFMSAttached();
-//            result = true;
-//
-//            station.isAutonomous();
-//            returns(true, false);
-//        }};
-//
-//        new Expectations(Timer.class) {{
-//            Timer.getMatchTime();
-//            returns(15D, 135D, 30D, 29D, 0D);
-//        }};
-//
-//        // Need to initialize the controller.
-//        blingController.initialize();
-//
-//
-//        assertEquals(BlingController.Period.AUTONOMOUS, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
-//    }
+            station.isAutonomous();
+            returns(true, false);
+        }};
+
+        new Expectations(Timer.class) {{
+            Timer.getMatchTime();
+            returns(15D, 135D, 30D, 29D, 0D);
+        }};
+
+        // Need to initialize the controller.
+        blingController.initialize();
+
+
+        assertEquals(BlingController.Period.AUTONOMOUS, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
+    }
 //
 //    /**
 //     * Tests getting the robot's operation period while not in a real match.

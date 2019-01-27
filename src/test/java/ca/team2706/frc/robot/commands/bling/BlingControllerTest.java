7919eaf0988dcda1fac249c43eedda6f113fec48
eandr127 <eandr127@outlook.com>
2019-01-26 21:28:12 -0500

    Revert to original tests

--- src/test/java/ca/team2706/frc/robot/commands/bling/BlingControllerTest.java
+++ src/test/java/ca/team2706/frc/robot/commands/bling/BlingControllerTest.java
@@ -1,16 +1,18 @@
 package ca.team2706.frc.robot.commands.bling;
 
+import ca.team2706.frc.robot.subsystems.Bling;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
-import mockit.*;
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.Tested;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static junit.framework.TestCase.assertEquals;
-
+import static org.junit.Assert.assertEquals;
 
 public class BlingControllerTest {
+
     @Tested
     private BlingController blingController;
 
@@ -18,98 +20,88 @@ public class BlingControllerTest {
     @Mocked
     private DriverStation station;
 
+    @Before
+    public void setUp() {
+        blingController = new BlingController();
+    }
+
     /**
      * Tests getting the robot's operation period while not in a real match.
      */
     @Test
     public void testGetOperationPeriodDuringRealMatch() {
-        new MockDriverStation();
+        new Expectations() {{
+            station.isFMSAttached();
+            result = true;
 
-//        new Expectations(DriverStation.class) {{
-//            DriverStation.getInstance();
-//            result = station;
-//        }};
-//
-//        new Expectations() {{
-//            station.isFMSAttached();
-//            result = true;
-//
-//            station.isAutonomous();
-//            returns(true, false);
-//        }};
-//
-//        new Expectations(Timer.class) {{
-//            Timer.getMatchTime();
-//            returns(15D, 135D, 30D, 29D, 0D);
-//            times = 5;
-//        }};
-//
-//        // Need to initialize the controller.
-//        blingController.initialize();
-//
-//
-//        assertEquals(BlingController.Period.AUTONOMOUS, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
+            station.isAutonomous();
+            returns(true, false);
+        }};
+
+        new Expectations(Timer.class) {{
+            Timer.getMatchTime();
+            returns(15D, 135D, 30D, 29D, 0D);
+        }};
+
+        // Need to initialize the controller.
+        blingController.initialize();
+
+
+        assertEquals(BlingController.Period.AUTONOMOUS, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
     }
-//
-//    /**
-//     * Tests getting the robot's operation period while not in a real match.
-//     */
-//    @Test
-//    public void testGetOperationPeriodDuringPractice() {
-//        new Expectations() {{
-//            station.isFMSAttached();
-//            result = false;
-//
-//            station.isAutonomous();
-//            returns(true, false);
-//        }};
-//
-//        new Expectations(Timer.class) {{
-//           Timer.getFPGATimestamp();
-//           // First one is to get the start time.
-//           returns(0D, 0D, 15D, 120D);
-//        }};
-//
-//        blingController.initialize();
-//
-//        assertEquals(BlingController.Period.AUTONOMOUS, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
-//    }
-//
-//    /**
-//     * Tests starting the robot in teleop and having the bling detect the period properly.
-//     */
-//    @Test
-//    public void testGetOperationPeriodStartingInTeleop() {
-//        new Expectations() {{
-//            station.isFMSAttached();
-//            result = false;
-//
-//            station.isAutonomous();
-//            result = false;
-//        }};
-//
-//        new Expectations(Timer.class) {{
-//            Timer.getFPGATimestamp();
-//            // First one is start time, next one is teleop time, next one is climb.
-//            returns(0D, 0D, 106D);
-//        }};
-//
-//        blingController.initialize();
-//
-//        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
-//        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
-//    }
 
-    private class MockDriverStation extends MockUp<DriverStation> {
-        @Mock
-        private void $init() {
-            System.out.println("Hit mocked constructor.");
-        }
+    /**
+     * Tests getting the robot's operation period while not in a real match.
+     */
+    @Test
+    public void testGetOperationPeriodDuringPractice() {
+        new Expectations() {{
+            station.isFMSAttached();
+            result = false;
+
+            station.isAutonomous();
+            returns(true, false);
+        }};
+
+        new Expectations(Timer.class) {{
+           Timer.getFPGATimestamp();
+           // First one is to get the start time.
+           returns(0D, 0D, 15D, 120D);
+        }};
+
+        blingController.initialize();
+
+        assertEquals(BlingController.Period.AUTONOMOUS, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
+    }
+
+    /**
+     * Tests starting the robot in teleop and having the bling detect the period properly.
+     */
+    @Test
+    public void testGetOperationPeriodStartingInTeleop() {
+        new Expectations() {{
+            station.isFMSAttached();
+            result = false;
+
+            station.isAutonomous();
+            result = false;
+        }};
+
+        new Expectations(Timer.class) {{
+            Timer.getFPGATimestamp();
+            // First one is start time, next one is teleop time, next one is climb.
+            returns(0D, 0D, 106D);
+        }};
+
+        blingController.initialize();
+
+        assertEquals(BlingController.Period.TELEOP_WITHOUT_CLIMB, blingController.getCurrentPeriod());
+        assertEquals(BlingController.Period.CLIMB, blingController.getCurrentPeriod());
     }
 }

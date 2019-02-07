f649367e064368443884de83eb36f8394f836ccf
Kyle Anderson <kra.aue@gmail.com>
2019-02-07 12:29:03 -0500

    Improve config tests
    The config tests are now a little better as they stub out the Config class a little earlier and they also
    test more reading of the robot ID.

--- src/test/java/ca/team2706/frc/robot/config/ConfigTest.java
+++ src/test/java/ca/team2706/frc/robot/config/ConfigTest.java
@@ -8,7 +8,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.nio.file.Files;
@@ -20,11 +19,20 @@ import static org.junit.Assert.assertTrue;
 
 public class ConfigTest {
     private Robot robot;
+
+    // We want to use the temporary directory for storing test files. We could mock the BufferedReader but that's not as good as the actual thing.
     private static final Path SAVE_FILE = Paths.get(System.getProperty("java.io.tmpdir"), "FluidConstants.txt.tmp");
-    private static final Path ID_FILE = Paths.get(System.getProperty("java.io.tmpdir"), "RobotID.txt.tmp");
+    private static final Path ID_FILE = Paths.get(System.getProperty("java.io.tmpdir"), "RobotID.txt.conf");
 
     @Before
-    public void setUp()  {
+    public void setUp() throws Exception {
+        // Need to set the save file constant to a temporary file location.
+        Field fileField = Config.class.getDeclaredField("SAVE_FILE");
+        setFinalStatic(fileField, SAVE_FILE);
+
+        Field idField = Config.class.getDeclaredField("ROBOT_ID_LOC");
+        setFinalStatic(idField, ID_FILE);
+
         robot = new Robot();
     }
 
@@ -32,10 +40,7 @@ public class ConfigTest {
      * Tests to ensure that fluid constants are saved to file when disabling the robot.
      */
     @Test
-    public void testSaveConstants() throws Exception {
-        // Neeed to set the save file constant to a temporary file location.
-        Field fileField = Config.class.getDeclaredField("SAVE_FILE");
-        setFinalStatic(fileField, SAVE_FILE);
+    public void testSaveConstants() {
 
         robot.disabledInit();
 
@@ -44,22 +49,31 @@ public class ConfigTest {
 
     /**
      * Tests to ensure that the robot's id is retrieved properly.
+     *
      * @throws Exception Java reflection exception.
      */
     @Test
     public void testGetRobotID() throws Exception {
-        Field idField = Config.class.getDeclaredField("ROBOT_ID_LOC");
-        setFinalStatic(idField, ID_FILE);
-
         Method getIdMethod = Config.class.getDeclaredMethod("getRobotId");
         getIdMethod.setAccessible(true);
 
+        writeToRobotIdFile(1);
+        assertEquals(1, (int) getIdMethod.invoke(null));
+
+        writeToRobotIdFile(2);
+        assertEquals(2, (int) getIdMethod.invoke(null));
+    }
+
+    /**
+     * Writes the given integer robot ID to the robot ID file.
+     *
+     * @param robotID The robot ID to be written.
+     */
+    private void writeToRobotIdFile(final int robotID) {
         try (BufferedWriter writer = Files.newBufferedWriter(ID_FILE)) {
-            writer.write("1");
+            writer.write(String.valueOf(robotID));
         } catch (IOException ignored) {
         }
-
-        assertEquals(1, (int) getIdMethod.invoke(null));
     }
 
     /**

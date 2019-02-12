7dd77a3210f4510f1c3e656e5698491ca2e98241
carmen1234 <carmen.lamprecht1@gmail.com>
2019-02-11 20:25:52 -0500

    Drive forward with time command

---
+++ src/main/java/ca/team2706/frc/robot/commands/drivebase/DriveForwardWithTime.java
@@ -0,0 +1,41 @@
+package ca.team2706.frc.robot.commands.drivebase;
+
+import ca.team2706.frc.robot.subsystems.DriveBase;
+import edu.wpi.first.wpilibj.command.TimedCommand;
+
+/**
+ * Drives in a straight line for an amount of time
+ */
+public class DriveForwardWithTime extends TimedCommand {
+
+    final double speed;
+
+    /**
+     * Creates a drive forward with time command
+     * @param timeout makes it stop the command after a time in seconds
+     * @param speed the speed for the robot to drive
+     */
+    public DriveForwardWithTime(double timeout, double speed) {
+        super(timeout, DriveBase.getInstance());
+        this.speed = speed;
+    }
+
+
+    @Override
+    public void initialize()
+    {
+        DriveBase.getInstance().setOpenLoopVoltageMode();
+        DriveBase.getInstance().setBrakeMode(true);
+    }
+
+    @Override
+    public void execute() {
+        DriveBase.getInstance().arcadeDrive(speed, 0, false);
+    }
+
+    @Override
+    public void end() {
+        DriveBase.getInstance().setDisabledMode();
+    }
+
+}

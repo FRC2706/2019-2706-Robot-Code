d219110f164ef1fd325472032b2983404365591c
eandr127 <eandr127@outlook.com>
2019-02-13 21:18:21 -0500

    Recursively try to mirror non-mirrorable commands, add command(group) overloads

--- src/main/java/ca/team2706/frc/robot/commands/mirrorable/MirroredCommand.java
+++ src/main/java/ca/team2706/frc/robot/commands/mirrorable/MirroredCommand.java
@@ -1,6 +1,7 @@
 package ca.team2706.frc.robot.commands.mirrorable;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  * A command that can be mirrored in a certain way
@@ -8,6 +9,38 @@ import edu.wpi.first.wpilibj.command.Command;
 public abstract class MirroredCommand extends Command implements IMirrorable<Command> {
 
     private boolean mirrored;
+
+    public MirroredCommand() {
+        super();
+    }
+
+    public MirroredCommand(String name) {
+        super(name);
+    }
+
+    public MirroredCommand(double timeout) {
+        super(timeout);
+    }
+
+    public MirroredCommand(Subsystem subsystem) {
+        super(subsystem);
+    }
+
+    public MirroredCommand(String name, Subsystem subsystem) {
+        super(name, subsystem);
+    }
+
+    public MirroredCommand(double timeout, Subsystem subsystem) {
+        super(timeout, subsystem);
+    }
+
+    public MirroredCommand(String name, double timeout) {
+        super(name, timeout);
+    }
+
+    public MirroredCommand(String name, double timeout, Subsystem subsystem) {
+        super(name, timeout, subsystem);
+    }
 
     /**
      * Mirrors the command (doesn't do anything to an already mirrored command)
--- src/main/java/ca/team2706/frc/robot/commands/mirrorable/MirroredCommandGroup.java
+++ src/main/java/ca/team2706/frc/robot/commands/mirrorable/MirroredCommandGroup.java
@@ -14,6 +14,17 @@ public class MirroredCommandGroup extends CommandGroup implements IMirrorable<Co
     private boolean mirrored;
 
     /**
+     * See 
+     */
+    public MirroredCommandGroup() {
+        super();
+    }
+
+    public MirroredCommandGroup(String name) {
+        super(name);
+    }
+
+    /**
      * Gets the mirrored version of this command (doesn't mirror an already mirrored command)
      */
     public MirroredCommandGroup mirror() {
@@ -26,18 +37,11 @@ public class MirroredCommandGroup extends CommandGroup implements IMirrorable<Co
         try {
             Field m_commands_field = CommandGroup.class.getDeclaredField("m_commands");
             m_commands_field.setAccessible(true);
-            Vector<?> m_commands = (Vector<?>) m_commands_field.get(this);
 
             Field commandField = Class.forName("edu.wpi.first.wpilibj.command.CommandGroup$Entry").getDeclaredField("m_command");
             commandField.setAccessible(true);
 
-            for (Object entry : m_commands) {
-                Command command = (Command) commandField.get(entry);
-
-                if (command instanceof IMirrorable) {
-                    ((IMirrorable) command).mirror();
-                }
-            }
+            mirrorChild(this, m_commands_field, commandField);
 
             commandField.setAccessible(false);
             m_commands_field.setAccessible(false);
@@ -46,6 +50,20 @@ public class MirroredCommandGroup extends CommandGroup implements IMirrorable<Co
         }
 
         return this;
+    }
+
+    private static void mirrorChild(Command c, Field m_commands_field, Field commandField) throws IllegalAccessException {
+        Vector<?> m_commands = (Vector<?>) m_commands_field.get(c);
+        for (Object entry : m_commands) {
+            Command command = (Command) commandField.get(entry);
+
+            if (command instanceof IMirrorable) {
+                ((IMirrorable) command).mirror();
+            }
+            else if(command instanceof CommandGroup) {
+                mirrorChild(command, m_commands_field, commandField);
+            }
+        }
     }
 
     /**

a6c66cf2c371267d6804f6e534e47cd411d36b80
eandr127 <eandr127@outlook.com>
2019-03-12 17:31:55 -0400

    Increase slowmode throttle

--- src/test/java/ca/team2706/frc/robot/commands/drivebase/CurvatureDriveWithJoystickTest.java
+++ src/test/java/ca/team2706/frc/robot/commands/drivebase/CurvatureDriveWithJoystickTest.java
@@ -115,8 +115,8 @@ public class CurvatureDriveWithJoystickTest {
         curveDrive.end();
 
         new Verifications() {{
-            DriveBase.getInstance().curvatureDrive(0.25, 0, false);
-            DriveBase.getInstance().curvatureDrive(0.25, 0, false);
+            DriveBase.getInstance().curvatureDrive(0.5, 0, false);
+            DriveBase.getInstance().curvatureDrive(0.5, 0, false);
         }};
     }
 }

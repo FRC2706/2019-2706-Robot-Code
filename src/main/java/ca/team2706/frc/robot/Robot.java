79e2fa6be7328be2dd176b56da784b3cb181248e
eandr127 <eandr127@outlook.com>
2019-03-15 16:18:13 -0400

    Only cancel non-default commands when in auto

--- src/main/java/ca/team2706/frc/robot/Robot.java
+++ src/main/java/ca/team2706/frc/robot/Robot.java
@@ -302,7 +302,7 @@ public class Robot extends TimedRobot {
      * Interrupt the current autonomous command and start teleop mode
      */
     public static void interruptCurrentCommand() {
-        if (currentCommand != null && currentCommand.isRunning()) {
+        if (currentCommand != null && currentCommand.isRunning() && DriverStation.getInstance().isAutonomous() && currentCommand != OI.getInstance().driveCommand && currentCommand != OI.getInstance().liftCommand) {
             DriverStation.reportWarning("Interupting auto", false);
             Log.w("Interrupting auto");
             currentCommand.cancel();

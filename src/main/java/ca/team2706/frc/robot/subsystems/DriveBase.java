73a4ee5a33ad75f9840c0504c065b1ed9015eeaf
eandr127 <eandr127@outlook.com>
2019-03-20 21:18:14 -0400

    Reset absolute gyro based on autonomous selector

--- src/main/java/ca/team2706/frc/robot/subsystems/DriveBase.java
+++ src/main/java/ca/team2706/frc/robot/subsystems/DriveBase.java
@@ -157,13 +157,6 @@ public class DriveBase extends Subsystem {
         motionProfilePointStreamRight = new BufferedTrajectoryPointStream();
 
         resetAbsoluteGyro();
-
-        // Reset absolute gyro when robot goes into autonomous for the first time in a real match
-        Robot.setOnStateChange(robotState -> {
-            if (robotState == RobotState.AUTONOMOUS && DriverStation.getInstance().isFMSAttached()) {
-                resetAbsoluteGyro();
-            }
-        });
     }
 
     /**
@@ -922,7 +915,15 @@ public class DriveBase extends Subsystem {
      * 0 degrees.
      */
     public void resetAbsoluteGyro() {
-        savedAngle = Config.ROBOT_START_ANGLE.value();
+        resetAbsoluteGyro(Config.ROBOT_START_ANGLE.value());
+    }
+
+    /**
+     * Resets the absolute gyro to a certain angle
+     * @param savedAngle The angle from 0 to 360
+     */
+    public void resetAbsoluteGyro(double savedAngle) {
+        this.savedAngle = savedAngle;
         gyro.setYaw(0, Config.CAN_SHORT);
     }
 

e758a6e98ece6d19e5dbbd75bf6ddc6bd3c56826
eandr127 <eandr127@outlook.com>
2019-03-21 19:18:51 -0400

    Fix tests passing

--- src/main/java/ca/team2706/frc/robot/commands/drivebase/CurvatureDriveWithJoystick.java
+++ src/main/java/ca/team2706/frc/robot/commands/drivebase/CurvatureDriveWithJoystick.java
@@ -26,6 +26,24 @@ public class CurvatureDriveWithJoystick extends CurvatureDrive {
     }
 
     /**
+     * Drives using axes that are on separate joysticks
+     *
+     * @param joy1    The joystick that the first axis is on
+     * @param axis1   The first axis
+     * @param invert1 Whether to negate the first joystick input
+     * @param joy2    The joystick that the second axis is on
+     * @param axis2   The second axis
+     * @param invert2 Whether to negate the second joystick input
+     * @param squareInputs Whether inputs should be squared
+     */
+    public CurvatureDriveWithJoystick(Joystick joy1, int axis1, boolean invert1,
+                                      Joystick joy2, int axis2, boolean invert2,
+                                      Joystick joy3, int button, boolean squareInputs) {
+        super(() -> sign(joy1.getRawAxis(axis1), invert1), () -> sign(joy2.getRawAxis(axis2), invert2),
+                Config.TELEOP_BRAKE, () -> joy3.getRawButton(button), squareInputs);
+    }
+
+    /**
      * Drives using axes that are on the same joystick
      *
      * @param joy     The joystick with the axes

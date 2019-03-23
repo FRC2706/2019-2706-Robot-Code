3c16dd46a3defbee34966445728865255ab3d93b
eandr127 <eandr127@outlook.com>
2019-03-22 22:42:06 -0400

    Complete vision trajectory generation

--- src/main/java/ca/team2706/frc/robot/commands/drivebase/VisionFollowTrajectory.java
+++ src/main/java/ca/team2706/frc/robot/commands/drivebase/VisionFollowTrajectory.java
@@ -18,11 +18,39 @@ public class VisionFollowTrajectory extends FollowTrajectory {
                         Config.VISION_ASSIST_MAX_JERK.value());
         Waypoint[] points = new Waypoint[]{
                 // Initial position/heading of robot: at origin with heading at 90 deg
-                new Waypoint(0, 0, Pathfinder.d2r(0)),
+                new Waypoint(0, 0, Pathfinder.d2r(90)),
                 // Final position/heading of robot: in front of target
                 //new Waypoint(vRobotToFinal_RobotX, vRobotToFinal_RobotY, angRobotHeadingFinalRad_Robot),
-                new Waypoint(left ? -1 : 1, 4, Pathfinder.d2r(0)),
+                new Waypoint(left ? -1 : 1, 4, Pathfinder.d2r(90)),
         };
-        return Pathfinder.generate(points, config);
+
+        Trajectory trajectory =  Pathfinder.generate(points, config);
+        /*
+         * Headings in trajectory must each be converted into the robot motion control system's frame
+         * whose x-y axes are the same as our robot frame but whose heading along the y-axis
+         * is at 0 degrees with  positive heading clockwise (as compared to our robot frame whose
+         * heading along the y-axis is 90 degrees with positive heading counter-clockwise).
+         */
+        double PI_OVER_2 = Math.PI / 2.0;
+        for (int i = 0; i < trajectory.length(); i++) {
+            trajectory.segments[i].heading = PI_OVER_2 - trajectory.segments[i].heading;
+        }
+
+
+        System.out.println("DAV: Trajectory length: " + trajectory.length());
+        for (int i = 0; i < trajectory.length(); i++)
+        {
+            String str =
+                    trajectory.segments[i].x + "," +
+                            trajectory.segments[i].y + "," +
+                            trajectory.segments[i].heading;
+
+            System.out.println(str);
+        }
+
+        System.out.println("DAV: Trajectory generated");
+
+        return trajectory;
+
     }
 }

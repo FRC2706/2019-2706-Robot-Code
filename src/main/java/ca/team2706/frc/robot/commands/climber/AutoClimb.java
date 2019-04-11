30ce4755c6f3c4f3c5d02a3d5950c5cc8e8fdada
eandr127 <eandr127@outlook.com>
2019-04-11 14:04:10 -0400

    Working auto climb
    The auto climb timings have been worked out and it's quite good.

--- src/main/java/ca/team2706/frc/robot/commands/climber/AutoClimb.java
+++ src/main/java/ca/team2706/frc/robot/commands/climber/AutoClimb.java
@@ -14,12 +14,13 @@ public class AutoClimb extends CommandGroup {
      * Constructs a new automatic command to climb to the habitation platform second level.
      */
     public AutoClimb() {
-        addSequential(new DriveForwardWithTime(0.25, -0.4));
+        addSequential(new DriveForwardWithTime(0.075, -0.2));
+        addSequential(new WaitCommand(0.2));
         addSequential(new MoveFrontClimberPistons(pneumaticState -> PneumaticState.DEPLOYED));
-        addSequential(new DriveForwardWithTime(0.25, 0.35)); // Needs to be accurate to avoid turning
+        addSequential(new DriveForwardWithTime(2, 0.2)); // Needs to be accurate to avoid turning
         addSequential(new MoveFrontClimberPistons(pneumaticState -> PneumaticState.STOWED));
-        addSequential(new DriveForwardWithTime(0.25, 0.4));
-        addParallel(new DriveForwardWithTime(Double.POSITIVE_INFINITY, 0.5));
+        addSequential(new DriveForwardWithTime(0.25, 0.25));
+        addParallel(new DriveForwardWithTime(Double.POSITIVE_INFINITY, 0.25));
         addSequential(new MoveBackClimberPistons(pneumaticState -> PneumaticState.DEPLOYED));
         addSequential(new WaitCommand("Climb Last Stage Delay", 1.0));
         addSequential(new MoveBackClimberPistons(pneumaticState -> PneumaticState.STOWED));

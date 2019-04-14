7797c1acfa850ea1b2592c0ec43c823403929a59
eandr127 <eandr127@outlook.com>
2019-04-14 10:57:19 -0400

    Decrease lift subsystem height

--- src/main/java/ca/team2706/frc/robot/subsystems/Lift.java
+++ src/main/java/ca/team2706/frc/robot/subsystems/Lift.java
@@ -37,7 +37,7 @@ public class Lift extends Subsystem {
      */
     private static final int[] HATCH_SETPOINTS = {
             0, // Bottom position
-            2000, // lowest hatch deploy
+            1725, // lowest hatch deploy
             27138, // middle hatch
             53500 // highest hatch
     };

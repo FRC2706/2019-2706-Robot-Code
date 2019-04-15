ba288ace42f377698a20f6b425311a4de3a9b047
Kyle Anderson <kra.aue@gmail.com>
2019-04-15 19:14:25 -0400

    Convert intake arms to new PneumaticController
    The intake arms' commands have been converted to a pneumatic controller command.

---
+++ src/main/java/ca/team2706/frc/robot/commands/intake/arms/MoveArms.java
@@ -0,0 +1,27 @@
+package ca.team2706.frc.robot.commands.intake.arms;
+
+import ca.team2706.frc.robot.commands.pneumatics.PneumaticController;
+import ca.team2706.frc.robot.config.Config;
+import ca.team2706.frc.robot.pneumatics.PneumaticState;
+import ca.team2706.frc.robot.subsystems.Pneumatics;
+
+import java.util.function.Supplier;
+
+/**
+ * Function for manipulating the intake arms pneumatics.
+ */
+public class MoveArms extends PneumaticController {
+
+    /**
+     * Constructs a command for moving the intake arms.
+     *
+     * @param desiredStateProvider The provider of the desired state of the pneumatics.
+     *                             The expected return is the desired position for the arms.
+     */
+    public MoveArms(final Supplier<PneumaticState> desiredStateProvider) {
+        super(Pneumatics.getInstance()::raiseArms, Pneumatics.getInstance()::lowerArms,
+                pneumaticState -> desiredStateProvider.get(), null, Config.INTAKE_ARMS_DELAY,
+                Pneumatics.getInstance()::stopArmsPneumatics);
+        requires(Pneumatics.getInstance());
+    }
+}

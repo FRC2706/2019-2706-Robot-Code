c7e257c5484da073763985070c23df8eb7cf3c2d
Kyle Anderson <kra.aue@gmail.com>
2019-04-18 17:08:57 -0400

    Resolve issue with always firing pneumatics
    It is now possible to specify to always fire pneumatics, even if they are already in position,
    however this isn't the default behaviour.
    This should be fine because this has always been the case.
    
    The problem with always firing them is that you need to turn them off after the time interval,
    and that would mean either making the command always run for the move time even if the stuff
    is already in position, or you would have to get creative.

--- src/main/java/ca/team2706/frc/robot/commands/pneumatics/PneumaticController.java
+++ src/main/java/ca/team2706/frc/robot/commands/pneumatics/PneumaticController.java
@@ -22,6 +22,43 @@ public class PneumaticController extends TimedCommand {
     private boolean isAlreadyInPosition;
 
     /**
+     * Set to true to always move the piston, even if it is already in the desired position.
+     */
+    private boolean alwaysMovePiston;
+
+    /**
+     * Constructs a generic pneumatic controller command for controlling a pneumatic piston.
+     *
+     * @param moveFunction         The function for moving the piston, which accepts an argument of the piston's
+     *                             desired state.
+     *                             Cannot be null.
+     * @param desiredStateProvider The supplier of the desired state of the piston.
+     *                             The provided argument is the current state of the piston.
+     *                             The expected return value is the desired state of the piston.
+     *                             Cannot be null or return null.
+     * @param currentStateProvider The supplier for the piston's current state. Can be null, but must not return null.
+     * @param moveTime             The time that should be allotted for moving the piston, if it is necessary to move it.
+     * @param stopFunction         The function for turning off the pneumatics. Cannot be null.
+     * @param alwaysMovePiston     True to continue with the command and move the piston even if it is already
+     *                             in the desired position.
+     */
+    public PneumaticController(final Consumer<PneumaticState> moveFunction,
+                               final Function<PneumaticState, PneumaticState> desiredStateProvider,
+                               final Supplier<PneumaticState> currentStateProvider,
+                               final double moveTime,
+                               final Runnable stopFunction,
+                               final boolean alwaysMovePiston) {
+        super(moveTime);
+
+        this.moveFunction = Objects.requireNonNull(moveFunction, "Move Function cannot be null.");
+        this.desiredStateProvider =
+                Objects.requireNonNull(desiredStateProvider, "Desired state supplier cannot be null.");
+        this.currentStateSupplier = currentStateProvider;
+        this.stopFunction = Objects.requireNonNull(stopFunction, "Stop function cannot be null.");
+        setAlwaysMovePiston(alwaysMovePiston);
+    }
+
+    /**
      * Constructs a generic pneumatic controller command for controlling a pneumatic piston.
      *
      * @param moveFunction         The function for moving the piston, which accepts an argument of the piston's
@@ -40,13 +77,7 @@ public class PneumaticController extends TimedCommand {
                                final Supplier<PneumaticState> currentStateProvider,
                                final double moveTime,
                                final Runnable stopFunction) {
-        super(moveTime);
-
-        this.moveFunction = Objects.requireNonNull(moveFunction, "Move Function cannot be null.");
-        this.desiredStateProvider =
-                Objects.requireNonNull(desiredStateProvider, "Desired state supplier cannot be null.");
-        this.currentStateSupplier = currentStateProvider;
-        this.stopFunction = Objects.requireNonNull(stopFunction, "Stop function cannot be null.");
+        this(moveFunction, desiredStateProvider, currentStateProvider, moveTime, stopFunction, false);
     }
 
     /**
@@ -86,6 +117,45 @@ public class PneumaticController extends TimedCommand {
     }
 
     /**
+     * Constructs a generic pneumatic controller command for controlling a pneumatic piston.
+     *
+     * @param deployFunction       The function for deploying (extending) the pneumatic piston.
+     *                             Can be null if deploying isn't desired behaviour.
+     * @param stowFunction         The function for retracting (stowing) the pneumatic piston.
+     *                             Can be null if stowing isn't desired behaviour.
+     * @param desiredStateProvider The supplier of the desired state of the piston.
+     *                             The provided argument is the current state of the piston.
+     *                             The expected return value is the desired state of the piston.
+     * @param currentStateProvider The supplier for the piston's current state.
+     * @param moveTime             The time that should be allotted for moving the piston, if it is necessary to move it.
+     * @param stopFunction         The function for turning off the pneumatics.
+     * @param alwaysMovePiston     True to continue with the command and move the piston even if it is already
+     *                             in the desired position.
+     */
+    public PneumaticController(final Runnable deployFunction,
+                               final Runnable stowFunction,
+                               final Function<PneumaticState, PneumaticState> desiredStateProvider,
+                               final Supplier<PneumaticState> currentStateProvider,
+                               final double moveTime,
+                               final Runnable stopFunction,
+                               final boolean alwaysMovePiston) {
+        this(pneumaticState -> {
+            switch (pneumaticState) {
+                case DEPLOYED:
+                    if (deployFunction != null) {
+                        deployFunction.run();
+                    }
+                    break;
+                case STOWED:
+                    if (stowFunction != null) {
+                        stowFunction.run();
+                    }
+                    break;
+            }
+        }, desiredStateProvider, currentStateProvider, moveTime, stopFunction, alwaysMovePiston);
+    }
+
+    /**
      * Constructs a pneumatic controller command for moving a pneumatic piston.
      *
      * @param moveFunction         The function for moving the piston, which accepts an argument of the piston's
@@ -104,6 +174,26 @@ public class PneumaticController extends TimedCommand {
         this(moveFunction, desiredStateSupplier, null, moveTime, stopFunction);
     }
 
+    /**
+     * Determines if this command is set to always move the piston, even if
+     * it is in the correct position.
+     *
+     * @return True if the command will always move the piston, false otherwise.
+     */
+    public boolean willAlwaysMovePiston() {
+        return alwaysMovePiston;
+    }
+
+    /**
+     * Sets whether or not the command will always move the piston.
+     *
+     * @param alwaysMovePiston True to move the piston even if it is in position already (and thereby
+     *                         making the command delay if the piston is in its correct position), false otherwise.
+     */
+    public void setAlwaysMovePiston(boolean alwaysMovePiston) {
+        this.alwaysMovePiston = alwaysMovePiston;
+    }
+
 
     @Override
     protected void initialize() {
@@ -113,16 +203,15 @@ public class PneumaticController extends TimedCommand {
         final PneumaticState desiredState = desiredStateProvider.apply(previousState);
 
         isAlreadyInPosition = previousState == desiredState;
-        moveFunction.accept(desiredState);
+
+        if (willAlwaysMovePiston() || !isAlreadyInPosition) {
+            moveFunction.accept(desiredState);
+        }
     }
 
     @Override
     protected boolean isFinished() {
-        /*
-        TODO if the piston is already in position, it will be turned on and then off instantaneously.
-        Need to determine what the desired behaviour for this is.
-        */
-        return super.isFinished() || isAlreadyInPosition;
+        return super.isFinished() || (!willAlwaysMovePiston() && isAlreadyInPosition);
     }
 
     @Override

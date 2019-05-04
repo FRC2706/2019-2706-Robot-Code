298e80ed34a95c88fd6d23395769703e44a02e15
Kyle Anderson <kra.aue@gmail.com>
2019-05-04 18:13:38 -0400

    Fix syntax problem
    Small syntax problem, fixed it.

--- src/main/java/ca/team2706/frc/robot/subsystems/Lift.java
+++ src/main/java/ca/team2706/frc/robot/subsystems/Lift.java
@@ -317,7 +317,7 @@ public class Lift extends Subsystem {
             }
 
             liftMotor.configClosedLoopPeakOutput(FEED_FORWARD_SLOT, 1.0); // Peak output to max (1.0).
-            liftMotor.selectProfileSlot(FEED_FORWARD_SLOT, 0, );
+            liftMotor.selectProfileSlot(FEED_FORWARD_SLOT, 0);
             liftMotor.set(ControlMode.Velocity, velocity, DemandType.ArbitraryFeedForward, Config.LIFT_FEED_FORWARD);
         }
     }
